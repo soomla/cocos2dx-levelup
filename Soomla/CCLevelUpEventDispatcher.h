c04597068d059fd6691382bebf77411678432d7f
sluzky <simon@soom.la>
2015-03-30 18:12:51 +0300

    adding events documentation

--- Soomla/CCLevelUpEventDispatcher.h
+++ Soomla/CCLevelUpEventDispatcher.h
@@ -42,23 +42,129 @@ namespace soomla {
 
         bool init();
 
+        /**
+         Fired when initialization has been completed.
+         
+         Event Name - CCLevelUpConsts::EVENT_LEVEL_UP_INITIALIZED
+         */
         virtual void onLevelUpInitialized();
 
+        /**
+         Fired when a CCGate has been opened.
+         
+         Event Name - CCLevelUpConsts::EVENT_GATE_OPENED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_GATE - CCGate - The gate which was opened
+         */
         virtual void onGateOpened(CCGate *gate);
+        /**
+         Fired when a CCGate has been closed.
+         
+         Event Name - CCLevelUpConsts::EVENT_GATE_CLOSED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_GATE - CCGate - The gate which was closed
+         balance has changed.
+         */
         virtual void onGateClosed(CCGate *gate);
 
+        /**
+         Fired when a CCMission has been completed.
+         
+         Event Name - CCLevelUpConsts::EVENT_MISSION_COMPLETED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_MISSION - CCMission - the mission that 
+         was completed.
+         */
         virtual void onMissionCompleted(CCMission *mission);
+        /**
+         Fired when CCMission completion has been revoked.
+         For example, you can decide to revoke a mission if the condition for 
+         completing it is no longer valid
+         
+         Event Name - CCLevelUpConsts::EVENT_MISSION_COMPLETION_REVOKED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_MISSION - CCMission - the mission to be
+         revoked.
+         */
         virtual void onMissionCompletionRevoked(CCMission *mission);
 
+        /**
+         Fired when a latest score is changed.
+         
+         Event Name - CCLevelUpConsts::EVENT_SCORE_RECORD_CHANGED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_SCORE - CCScore - the score which has 
+         been changed.
+         */
         virtual void onLatestScoreChanged(CCScore *score);
+        /**
+         Fired when a new record has been reached for a score.
+         
+         Event Name - CCLevelUpConsts::EVENT_SCORE_RECORD_REACHED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_SCORE - CCScore - the score which has 
+         reached a new record.
+         */
         virtual void onScoreRecordReached(CCScore *score);
+        /**
+         Fired when a score's record is changed.
+         
+         Event Name - CCLevelUpConsts::EVENT_SCORE_RECORD_CHANGED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_SCORE - CCScore - the score which 
+         has been changed.
+         */
         virtual void onScoreRecordChanged(CCScore *score);
 
+        /**
+         Fired when a CCWorld has been completed.
+         
+         Event Name - CCLevelUpConsts::EVENT_WORLD_COMPLETED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_WORLD - CCWorld - the world which was 
+         completed.
+         */
         virtual void onWorldCompleted(CCWorld *world);
+        /**
+         Fired when the last completed world inside a world has changed.
+         
+         Event Name - CCLevelUpConsts::EVENT_WORLD_LAST_COMPLETED_INNER_WORLD_CHANGED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_WORLD - CCWorld - the world which had 
+         last completed world changed.
+         CCLevelUpConsts::DICT_ELEMENT_INNER_WORLD - __String - The inner world 
+         ID which was last completed.
+         */
         virtual void onLastCompletedInnerWorldChanged(CCWorld *world, cocos2d::__String *innerWorldId);
+        /**
+         Fired when a CCWorld is being assigned a reward.
+         
+         Event Name - CCLevelUpConsts::EVENT_WORLD_REWARD_ASSIGNED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::DICT_ELEMENT_WORLD - CCWorld - the World whose
+         reward has changed.
+         */
         virtual void onWorldRewardAssigned(CCWorld *world);
 
+        /**
+         Fired when a level has started, i.e. when start is called on an 
+         instance of CCLevel
+         
+         Event Name - CCLevelUpConsts::EVENT_GOOD_BALANCE_CHANGED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::EVENT_LEVEL_STARTED - CCLevel - the level which just 
+         started.
+         */
         virtual void onLevelStarted(CCLevel *level);
+        /**
+         Fired when a level has ended, i.e. when end is called on an
+         instance of CCLevel
+         
+         Event Name - CCLevelUpConsts::EVENT_LEVEL_ENDED
+         Event Data (__Dictionary):
+         CCLevelUpConsts::EVENT_LEVEL_STARTED - CCLevel - the level which just 
+         ended.
+         */
         virtual void onLevelEnded(CCLevel *level);
     };
 }
